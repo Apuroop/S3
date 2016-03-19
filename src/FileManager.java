ca3bf79c9b0a1d566a14e9ff5e7516373b590fea
Apuroop <apuroop.sethupathy@ashoka.edu.in>
2016-03-19 17:22:05 +0530

    Section 4 completed.

--- src/FileManager.java
+++ src/FileManager.java
@@ -4,7 +4,7 @@
 import java.io.File;
 import java.lang.String;
 public class FileManager {
-    public void listDirectoryContents(String directoryPath){
+    public static void listDirectoryContents(String directoryPath){
         File directory = new File(directoryPath);
         File[] fileList = directory.listFiles();
         int subFiles=0;
@@ -21,7 +21,7 @@ public class FileManager {
             }
         }
     }
-    public int countFiles(String directoryPath){
+    public static int countFiles(String directoryPath){
         File directory = new File(directoryPath);
         File[] fileList = directory.listFiles();
         int fileCount=0;
@@ -32,7 +32,7 @@ public class FileManager {
         }
         return fileCount;
     }
-    public int countAllFiles(String directoryPath, int fileCount){
+    public static int countAllFiles(String directoryPath, int fileCount){
         File directory = new File(directoryPath);
         File[] fileList = directory.listFiles();
         for (File item : fileList){
@@ -45,10 +45,48 @@ public class FileManager {
         }
         return fileCount;
     }
-    public static void main (String args[]){
+    public int[] fileTypeCounter(String directoryPath, int[] typeCount){
+        String[] extTypes = new String[]{"jpg", "png", "gif", "mp4", "mp3", "exe", "psd", "html", "xml","txt"};
+        File directory = new File(directoryPath);
+        File[] fileList = directory.listFiles();
+        String extnCheck="";
+        for (File item : fileList){
+            if(item.isFile()){
+                extnCheck= truncateExtension(item.getAbsolutePath());
+                for(int i=0; i<extTypes.length; i++){
+                    if(extnCheck.equalsIgnoreCase(extTypes[i])){
+                        typeCount[i]++;
+                    }
+                }
+            }
+            else if(item.isDirectory()){
+                typeCount=fileTypeCounter(item.getAbsolutePath(), typeCount);
+            }
+        }
+        //System.out.println("NUMBER OF FILES BY TYPE");
+        //for (int j=0; j<extTypes.length; j++){
+        //    System.out.println(extTypes[j]+"\t"+typeCount[j]);
+        return typeCount;
+    }
+    public static String truncateExtension(String filePath){
+        String extn = "";
+        int i= filePath.lastIndexOf('.');
+        if(i>=0){
+            extn=filePath.substring(i+1);
+        }
+        return extn;
+    }
+    public static void main (String args[]) {
         FileManager fileManager = new FileManager();
+        int[] typeCount = new int[100];
+        String[] extTypes = new String[]{"jpg", "png", "gif", "mp4", "mp3", "exe", "psd", "html", "xml", "txt"};
         String directory = "C:\\Users\\Apuroop\\Videos";
         System.out.println("##\t#\tType\tPath");
         fileManager.listDirectoryContents(directory);
+        typeCount = fileManager.fileTypeCounter(directory, typeCount);
+        System.out.println("NUMBER OF FILES BY TYPE");
+        for (int j = 0; j < extTypes.length; j++) {
+            System.out.println(extTypes[j] + "\t" + typeCount[j]);
+        }
     }
 }
