4ace7d8f9d0b623413e52d979571634a726da89e
Apuroop <apuroop.sethupathy@ashoka.edu.in>
2016-03-19 23:45:07 +0530

    Section 5 partially completed. Need to figure out why it is being truncated. Comments added for the entire program.

--- src/FileManager.java
+++ src/FileManager.java
@@ -4,6 +4,8 @@
 import java.io.File;
 import java.lang.String;
 public class FileManager {
+    //This method takes in the Starting directory path (directoryPath) as it's input. It does not return any value.
+    //This method recursively prints the total number of files and folders by calling other methods defined
     public static void listDirectoryContents(String directoryPath){
         File directory = new File(directoryPath);
         File[] fileList = directory.listFiles();
@@ -21,6 +23,8 @@ public class FileManager {
             }
         }
     }
+    //This method takes in the Directory path as a string which is sent by listDirectoryContents.
+    // It returns the number of  files present in a particular folder. It does not check sub-folders
     public static int countFiles(String directoryPath){
         File directory = new File(directoryPath);
         File[] fileList = directory.listFiles();
@@ -32,6 +36,10 @@ public class FileManager {
         }
         return fileCount;
     }
+    //This method accepts 2 parameters, the directory path as a string and fileCount which stores the number of files
+    //that the method has counted previously. fileCount is initially zero, and gets updated every time countAllFiles
+    //is recursively called. The return value is the total number of files present within a folder. This does check
+    //all subfolders present in a particular file.
     public static int countAllFiles(String directoryPath, int fileCount){
         File directory = new File(directoryPath);
         File[] fileList = directory.listFiles();
@@ -45,17 +53,24 @@ public class FileManager {
         }
         return fileCount;
     }
-    public int[] fileTypeCounter(String directoryPath, int[] typeCount){
+    //This method accepts a string and a 2D long as the parameters. the string provides the directoryPath of the file currently
+    //being checked. The 2D long stores 2 values. the first row is devoted to keeping track of number of files of each kind that are
+    //present. the second row is devoted to keeping track of the total size of files of each type. this method returns a 2D long
+    //array which contains all the updated stats for each file.
+    public static long [][] fileTypeCounter(String directoryPath, long[][] typeCount){
         String[] extTypes = new String[]{"jpg", "png", "gif", "mp4", "mp3", "exe", "psd", "html", "xml","txt"};
         File directory = new File(directoryPath);
         File[] fileList = directory.listFiles();
+        long tempSize=0;
         String extnCheck="";
         for (File item : fileList){
             if(item.isFile()){
+                tempSize= fileSize(item.getAbsolutePath());
                 extnCheck= truncateExtension(item.getAbsolutePath());
                 for(int i=0; i<extTypes.length; i++){
                     if(extnCheck.equalsIgnoreCase(extTypes[i])){
-                        typeCount[i]++;
+                        typeCount[0][i]++;
+                        typeCount[1][i]=typeCount[1][i]+tempSize;
                     }
                 }
             }
@@ -63,11 +78,10 @@ public class FileManager {
                 typeCount=fileTypeCounter(item.getAbsolutePath(), typeCount);
             }
         }
-        //System.out.println("NUMBER OF FILES BY TYPE");
-        //for (int j=0; j<extTypes.length; j++){
-        //    System.out.println(extTypes[j]+"\t"+typeCount[j]);
         return typeCount;
     }
+    //this method takes in a string as its parameter, which contains the absolute path of the file that is being sent.
+    //the return type of this is a string which contains only the extension of the file. No folders can be sent to this method.
     public static String truncateExtension(String filePath){
         String extn = "";
         int i= filePath.lastIndexOf('.');
@@ -76,9 +90,15 @@ public class FileManager {
         }
         return extn;
     }
+    //this method takes in a string as its parameter which contains the absolute path of a file.
+    //it returns the size of the file as a long value.
+    public static long fileSize(String filePath){
+        return filePath.length();
+    }
     public static void main (String args[]) {
+        long t0= System.currentTimeMillis();
         FileManager fileManager = new FileManager();
-        int[] typeCount = new int[100];
+        long [][] typeCount = new long [2][20];
         String[] extTypes = new String[]{"jpg", "png", "gif", "mp4", "mp3", "exe", "psd", "html", "xml", "txt"};
         String directory = "C:\\Users\\Apuroop\\Videos";
         System.out.println("##\t#\tType\tPath");
@@ -86,7 +106,9 @@ public class FileManager {
         typeCount = fileManager.fileTypeCounter(directory, typeCount);
         System.out.println("NUMBER OF FILES BY TYPE");
         for (int j = 0; j < extTypes.length; j++) {
-            System.out.println(extTypes[j] + "\t" + typeCount[j]);
+            System.out.println(extTypes[j] + "\t\t" + typeCount[0][j]+"\t\t"+typeCount[1][j]);
         }
+        long t1= System.currentTimeMillis();
+        System.out.println("TOTAL TIME TAKEN = "+(t1-t0));
     }
 }
